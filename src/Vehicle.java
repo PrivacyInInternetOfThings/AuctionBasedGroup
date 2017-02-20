febd8791242af024be78574f3a5bee14320b72cd
Tuna Han Salih Meral <tunahansmeral@gmail.com>
2017-02-20 14:59:23 +0300

    added

---
+++ src/Vehicle.java
@@ -0,0 +1,181 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Random;
+
+public class Vehicle {
+
+	public VEHICLETYPE vehicleType;
+	public EMERGENCYTYPE emergencyType;
+	public MALFUNCTIONTYPE malfunctionType;
+	public int numOfPeople;
+	public double lostPrivacy;
+
+	// public double vehiclePrivacy;
+	// public double emergencyPrivacy;
+	// public double malfunctionPrivacy;
+	// public double peoplePrivacy;
+	public double[] privacy = new double[4];
+
+	public boolean vehicleTypeEnabled = false;
+	public boolean emergencyTypeEnabled = false;
+	public boolean malfunctionTypeEnabled = false;
+	public boolean numOfPeopleEnabled = false;
+	public boolean[] enabled = new boolean[4];
+
+	public double utility;
+
+	public Vehicle(VEHICLETYPE vehicle, EMERGENCYTYPE emergency, MALFUNCTIONTYPE malfunction, int num) {
+		this.vehicleType = vehicle;
+		this.emergencyType = emergency;
+		this.malfunctionType = malfunction;
+		this.numOfPeople = num;
+		this.utility = 0;
+		this.lostPrivacy = 0;
+		setPrivacyRandom();
+		for (int i = 0; i < 4; i++) {
+			System.out.print(privacy[i] + " ");
+		}
+		System.out.println();
+		System.out.println();
+	}
+
+	// public void setPrivacy(double vehicle, double emergency, double
+	// malfunction, double people) {
+	// this.vehiclePrivacy = vehicle;
+	// this.emergencyPrivacy = emergency;
+	// this.malfunctionPrivacy = malfunction;
+	// this.peoplePrivacy = people;
+	// }
+
+	public void clear() {
+		for (int i = 0; i < 4; i++) {
+			enabled[i] = false;
+		}
+		this.lostPrivacy = 0;
+		this.utility = 0;
+	}
+
+	public void setPrivacyRandom() {
+		Random rand = new Random();
+		// this.vehiclePrivacy = rand.nextDouble();
+		// this.emergencyPrivacy = rand.nextDouble();
+		// this.malfunctionPrivacy = rand.nextDouble();
+		// this.peoplePrivacy = rand.nextDouble();
+
+		double sum = 0;
+
+		for (int i = 0; i < 4; i++) {
+			this.privacy[i] = rand.nextDouble();
+			sum += this.privacy[i];
+		}
+		for (int i = 0; i < 4; i++) {
+			this.privacy[i] /= sum;
+		}
+	}
+
+	/**
+	 * 
+	 * @return indexes of properties that are not enabled
+	 */
+	public ArrayList<Integer> getEnabledIndex() {
+
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 0; i < enabled.length; i++) {
+			if (!enabled[i]) {
+				list.add(i);
+			}
+		}
+		return list;
+	}
+
+	public int getMinPrivacy() {
+		int minIndex = -1;
+		double minVal = 1;
+		ArrayList<Integer> list = getEnabledIndex();
+		for (int i = 0; i < list.size(); i++) {
+			if (privacy[i] < minVal) {
+				minIndex = list.get(i);
+				minVal = privacy[i];
+			}
+		}
+		if (minIndex >= 0)
+			enabled[minIndex] = true;
+
+		return minIndex;
+	}
+
+	public double makeOffer() {
+		int min = getMinPrivacy();
+		if (min == 0 && privacy[0] < 0.3) {
+			System.out.println("Vehicle Type privacy = " + privacy[0]);
+			this.lostPrivacy += privacy[0];
+			utility += this.vehicleType.getValue() * Main.proportionVehicleType;
+			return this.vehicleType.getValue() * Main.proportionVehicleType;
+		}
+		if (min == 1 && privacy[1] < 0.3) {
+			System.out.println("Emergency Type privacy = " + privacy[1]);
+			this.lostPrivacy += privacy[1];
+			utility += this.emergencyType.getValue() * Main.proportionEmergencyType;
+			return this.emergencyType.getValue() * Main.proportionEmergencyType;
+		}
+		if (min == 2 && privacy[2] < 0.3) {
+			System.out.println("Malfunction Type privacy = " + privacy[2]);
+			this.lostPrivacy += privacy[2];
+			utility += this.malfunctionType.getValue() * Main.proportionMalfunctionType;
+			return this.malfunctionType.getValue() * Main.proportionMalfunctionType;
+		}
+		if (min == 3 && privacy[3] < 0.3) {
+			System.out.println("Number of People privacy = " + privacy[3]);
+			this.lostPrivacy += privacy[3];
+			utility += this.numOfPeople / 50.0 * Main.proportionNumberPeople;
+			return this.numOfPeople / 50.0 * Main.proportionNumberPeople;
+		}
+		return 0;
+	}
+
+	public double makeOffer(double opponentOffer) {
+		double newOffer = 0;
+		double offerLostPrivacy = 0;
+		int min;
+		do {
+			min = getMinPrivacy();
+			if (min == 0) {
+				System.out.println("Vehicle Type privacy = " + privacy[0]);
+				offerLostPrivacy += privacy[0];
+				utility += this.vehicleType.getValue() * Main.proportionVehicleType;
+				newOffer += this.vehicleType.getValue() * Main.proportionVehicleType;
+			}
+			if (min == 1) {
+				System.out.println("Emergency Type privacy = " + privacy[1]);
+				offerLostPrivacy += privacy[1];
+				utility += this.emergencyType.getValue() * Main.proportionEmergencyType;
+				newOffer += this.emergencyType.getValue() * Main.proportionEmergencyType;
+			}
+			if (min == 2) {
+				System.out.println("Malfunction Type privacy = " + privacy[2]);
+				offerLostPrivacy += privacy[2];
+				utility += this.malfunctionType.getValue() * Main.proportionMalfunctionType;
+				newOffer += this.malfunctionType.getValue() * Main.proportionMalfunctionType;
+			}
+			if (min == 3) {
+				System.out.println("Number of People privacy = " + privacy[3]);
+				offerLostPrivacy += privacy[3];
+				utility += this.numOfPeople / 50.0 * Main.proportionNumberPeople;
+				newOffer += this.numOfPeople / 50.0 * Main.proportionNumberPeople;
+			}
+			if(min == -1){
+				break;
+			}
+		} while (newOffer < opponentOffer);
+		
+		if(newOffer < opponentOffer){
+			
+			return 0;
+		}
+		this.lostPrivacy += offerLostPrivacy;
+		return newOffer;
+
+
+	}
+
+}
